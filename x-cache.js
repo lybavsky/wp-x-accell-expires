b9945b2440ab1dffb295557721d35533369ae8e2
lybavsky <lybavsky@gmail.com>
2021-08-03 01:07:17 +0300

    some changes

--- x-cache.js
+++ x-cache.js
@@ -1,8 +1,125 @@
-function x_cache_init(){
-    var el=document.getElementById("testfield_js");
-    el.value="ttt"+el.value
+const DEFAULT_TTL = 60;
+
+const FIELDS_COUNT = 2;
+
+window.onload = function () {
+    field_rules = document.getElementById("x_cache_rules");
+    cont = document.getElementById("x_rules_cont");
+    submit_btn = document.getElementById("submit");
+
+
+    submit_btn.onclick = function (e) {
+        save_field()
+
+        for (var idx = 0; idx < x_rules.length; idx++) {
+            val = x_rules[idx];
+
+            if (val["ttl"]<0) {
+                alert("ttl ("+val["ttl"] + ") не должен быть меньше нуля")
+                e.preventDefault()
+                return;
+            }
+        }
+    }
+
+    load_field()
+    render_field()
 }
 
-window.onload = function (){
-    x_cache_init()
+var x_rules = []
+
+var field_rules = null
+var cont = null
+var submit_btn = null
+
+function render_field() {
+    var rules_html = "";
+
+    x_rules.forEach(function (x_rule, x_rule_idx) {
+        var rule_html = "<div style='margin-bottom: 5px'>" +
+            "<input id='x_rule_field_rule_" + x_rule_idx + "' type='text' value='" + x_rule["rule"] + "' onchange='save_x_rules()'>" +
+            "<input id='x_rule_field_ttl_" + x_rule_idx + "' type='number' value='" + x_rule["ttl"] + "' onchange='save_x_rules()'>" +
+            "<input id='x_rule_field_isregex_" + x_rule_idx + "' type='checkbox' " + (x_rule["isregex"]==="true" ? 'checked' : '') + " onchange='save_x_rules()' style='margin-left: 3px'>" +
+            "<button onclick='delete_row(" + x_rule_idx + ")'>del</button>"
+            + "</div>";
+        rules_html += rule_html;
+    })
+
+    rules_html += "<div>" +
+        "<input type='button' onclick='new_row()' value='add'>"
+    "</div>"
+
+    cont.innerHTML = rules_html;
+}
+
+//Загружаем значения ttl из сеттинга вордпресса
+function load_field() {
+    var rule_rows = field_rules.value.split("\n");
+
+    rule_rows.forEach(function (rule_row, rule_row_idx, arr) {
+        var col_split_idxs = []
+
+        for (var i = 0; i < rule_row.length; i++) {
+            if (rule_row[i] === ";" && (i === 0 || rule_row[i - 1] !== "\\")) {
+                col_split_idxs.push(i);
+            }
+        }
+        if (col_split_idxs.length !== FIELDS_COUNT) {
+            xmsg("Problem while parse value: " + rule_row);
+            return
+        }
+
+        var rule = ""
+        var ttl = 0
+        var isregex = false
+
+
+        var rule = rule_row.substring(0, col_split_idxs[0]).replaceAll("\\;",";");
+        var ttl = rule_row.substring(col_split_idxs[0] + 1, col_split_idxs[1])
+        var isregex = rule_row.substring(col_split_idxs[1] + 1,)
+
+        x_rules.push({"rule": rule, "ttl": ttl, "isregex": isregex});
+    })
+}
+
+function save_x_rules() {
+    for (var idx = 0; idx < x_rules.length; idx++) {
+        var val = x_rules[idx];
+        rule = document.getElementById("x_rule_field_rule_" + idx).value
+        ttl = document.getElementById("x_rule_field_ttl_" + idx).value
+        isregex = document.getElementById("x_rule_field_isregex_" + idx).checked
+        x_rules[idx] = {"rule": rule, "ttl": ttl, "isregex": isregex}
+    }
+
+    save_field()
+
+
+}
+
+//Сохраняем новые значения из наших полей в сеттинг вордпресса
+function save_field() {
+    var value = ""
+    x_rules.forEach(function (val, idx, arr) {
+        value += val["rule"].replaceAll(";","\\;") + ";" + val["ttl"] + ";" + val["isregex"] + (idx !== arr.length - 1 ? "\n" : "")
+    })
+
+    field_rules.value = value;
+
+}
+
+function delete_row(idx) {
+    x_rules.splice(idx, 1)
+    render_field()
+    save_field()
+}
+
+function new_row() {
+    x_rules.push({"rule": "http://example.com/.*", "ttl": DEFAULT_TTL, "isregex": false})
+    render_field()
+    save_field()
+}
+
+
+function xmsg(message) {
+    console.log("X-Cache-Plugin: " + message)
 }
--- x-cache.php
+++ x-cache.php
@@ -16,6 +16,10 @@ const CAP = "manage_options";
 const SLUG = "xcache";
 const SETTINGS_NAME = "xcache_settings";
 
+const OPTION_NAME = "x_cache_rules";
+
+const CACHE_TTL = 60;
+
 const SETTINGS_SECTION_ACCEL = "cache_accel";
 const SETTINGS_SECTION_DROP = "cache_drop";
 
@@ -28,7 +32,11 @@ class XCache
         add_action('admin_init', array($this, "setup_sections"));
         add_action('admin_init', array($this, "setup_fields"));
 
+
+        add_filter('wp_headers', array($this, "set_x_accel_expires"));
+
     }
+
 
     function show_menu()
     {
@@ -39,7 +47,10 @@ class XCache
     function page_callback($attributes)
     {
         ?>
+
+
         <script lang="js" src="<?= plugin_dir_url(__FILE__) . "x-cache.js" ?>"></script>
+
 
         <h1>
             <?= get_admin_page_title() ?>
@@ -47,12 +58,12 @@ class XCache
 
         <div>
             <form method="post" action="options.php">
-        <?php
-        settings_fields(SETTINGS_NAME);
-        do_settings_sections(SLUG);
-        submit_button();
-        ?>
-        </form>
+                <?php
+                settings_fields(SETTINGS_NAME);
+                do_settings_sections(SLUG);
+                submit_button();
+                ?>
+            </form>
         </div>
         <?php
     }
@@ -77,23 +88,59 @@ class XCache
 
     function setup_fields()
     {
-        add_settings_field("testfield", "Test text field", array($this, "setup_field_testfield"), SLUG, SETTINGS_SECTION_ACCEL);
-        register_setting(SETTINGS_NAME, "testfield");
 
-        add_settings_field("testfield_js", "Test js field", array($this, "setup_field_jsfield"), SLUG, SETTINGS_SECTION_ACCEL,array("id"=>"testfield_js"));
-        register_setting(SETTINGS_NAME, "testfield_js");
+        add_settings_field("x_cache_rules", "Cache rules", array($this, "setup_field_x_cache_rules"), SLUG, SETTINGS_SECTION_ACCEL);
+        register_setting(SETTINGS_NAME, OPTION_NAME);
 
     }
 
-    function setup_field_testfield($arguments)
+    function setup_field_x_cache_rules($arguments)
     {
-        echo "<input type='text' name='testfield' id='testfield' placeholder='some text' value='" . get_option("testfield") . "'/>";
+        ?>
+        <textarea style="display: none" type='text' name='<?= OPTION_NAME ?>'
+                  id='<?= OPTION_NAME ?>'><?= trim(get_option(OPTION_NAME)) ?></textarea>
+
+        <div id='x_rules_cont'>Cont</div>
+        <?php
     }
 
-    function setup_field_jsfield($arguments)
+
+    function set_x_accel_expires($headers)
     {
-        echo "<input type='text' name='" . $arguments['id'] . "' id='" . $arguments['id'] . "' placeholder='some text' value='" . get_option($arguments['id']) . "'/>";
+        $crules = wp_cache_get("x_cache_accel_array");
+
+        if ($crules == false) {
+            $crules = save_to_cache();
+        }
+
+        return $headers;
     }
 }
+
+function save_to_cache()
+{
+    $frules = get_option(OPTION_NAME);
+    $srules = explode("\r", $frules);
+
+
+    for ($r_idx = 0; $r_idx <= sizeof($srules); $r_idx++) {
+        $srule = $srules[$r_idx];
+        $idxs = [];
+
+        for ($i = 0; $i < strlen($srule); $i++) {
+            if ($srule[$i] === ";" && ($i === 0 || $srule[$i - 1] !== "\\")) {
+                array_push($idxs, $i);
+            }
+        }
+
+        $rule = substr($srule, 0, $idxs[0]);
+        $ttl = substr($srule, $idxs[0] + 1, $idxs[1] - $idxs[0] - 1);
+        $isregex = substr($srule, $idxs[1] + 1, strlen($srule));
+
+        error_log("rule: " . $srule . " -> " . $rule . " -> " . $ttl . " -> " . $isregex);
+    }
+
+}
+
 
 $xcache = new XCache();
